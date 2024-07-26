2144c48f49174079e95236e504759fcc543a0cd8
Neeraj <neerajsaraswat111@gmail.com>
2024-07-26 10:06:07 +0530

    added loading context

---
+++ src/LoadingContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useContext } from "react";
+
+const LoadingContext = createContext();
+
+export const LoadingProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = () => {
+  return useContext(LoadingContext);
+};
