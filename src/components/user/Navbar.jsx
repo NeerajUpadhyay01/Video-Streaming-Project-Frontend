adba404ca3310b6b9202ee7ee7afd93081a26ef1
Neeraj <neerajsaraswat111@gmail.com>
2024-07-26 12:33:25 +0530

    default user avatar added

--- src/components/user/Navbar.jsx
+++ src/components/user/Navbar.jsx
@@ -52,7 +52,7 @@ function Navbar(props) {
       </form>
       <img
         id="avatar"
-        src={avatar ? avatar : "/icons8-user-64.png"}
+        src={avatar ? avatar : "/icons8-user-64.webp"}
         alt="avatar"
         onClick={() => props.toggleMenu()}
       />
