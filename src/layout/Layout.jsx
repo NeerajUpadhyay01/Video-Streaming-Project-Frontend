945fba79a454a069fb2e9b45e05667b7b313f7a0
Neeraj <neerajsaraswat111@gmail.com>
2024-12-13 10:35:37 +0530

    revert changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,23 +1,12 @@
-// src/components/Layout.jsx
-import React, { useEffect } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-import { setupInterceptors } from "../utils/axiosInterceptor";
 
 function Layout() {
-  const { refreshToken, loading } = useAuth();
-
-  useEffect(() => {
-    // Setup axios interceptors
-    setupInterceptors(refreshToken);
-  }, [refreshToken]);
-
-  if (loading) {
-    // Optional: Add a loading spinner or placeholder
-    return <div>Loading...</div>;
-  }
-
-  return <Outlet />;
+  return (
+    <>
+        <Outlet/>
+    </>
+  );
 }
 
 export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { AuthProvider } from "./contexts/AuthContext";
 import {
   Route,
   RouterProvider,
@@ -85,8 +84,6 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
