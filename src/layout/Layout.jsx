29c1428446c0f6020df434103d9ebfcd1b3ba06e
Neeraj <neerajsaraswat111@gmail.com>
2025-03-10 10:05:39 +0530

    added logic related to refresh-token route

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,22 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const refreshAccessToken = async () => {
+            try {
+                await axios.post(`${server}/refresh-token`, {}, { withCredentials: true });
+            } catch (error) {
+                console.error("Error refreshing token", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        refreshAccessToken();
+    }, []);
+
   return (
     <>
         <Outlet/>
