7d195d6494ba7aa35609966d882f68405cb3cc7f
Neeraj <neerajsaraswat111@gmail.com>
2025-03-10 10:12:53 +0530

    made some changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { useState, useEffect, axios, server } from "./imports.jsx";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const refreshAccessToken = async () => {
-            try {
-                await axios.post(`${server}/refresh-token`, {}, { withCredentials: true });
-            } catch (error) {
-                console.error("Error refreshing token", error);
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const refreshAccessToken = async () => {
+      try {
+        await axios.post(
+          `${server}/refresh-token`,
+          {},
+          { withCredentials: true }
+        );
+      } catch (error) {
+        console.error("Error refreshing token", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        refreshAccessToken();
-    }, []);
+    refreshAccessToken();
+  }, []);
 
   return (
     <>
-        <Outlet/>
+      <Outlet />
     </>
   );
 }
