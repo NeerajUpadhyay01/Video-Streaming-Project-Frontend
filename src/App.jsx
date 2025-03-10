e1f449cabedd567c1024dc0968a3019ed670a6a7
Neeraj <neerajsaraswat111@gmail.com>
2025-03-10 10:48:09 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
+import { useEffect, axios, server } from "./imports.jsx";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+  useEffect(() => {
+    const refreshAccessToken = async () => {
+      try {
+        await axios.post(
+          `${server}/refresh-token`,
+          {},
+          { withCredentials: true }
+        );
+      } catch (error) {
+        console.error("Error refreshing token", error);
+      }
+    };
+
+    refreshAccessToken();
+  }, []);
+  return <></>;
 }
 
 export default App;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { useEffect, axios, server } from "../imports.jsx";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
-  useEffect(() => {
-    const refreshAccessToken = async () => {
-      try {
-        await axios.post(
-          `${server}/refresh-token`,
-          {},
-          { withCredentials: true }
-        );
-      } catch (error) {
-        console.error("Error refreshing token", error);
-      }
-    };
-
-    refreshAccessToken();
-  });
-
   return (
     <>
       <Outlet />
