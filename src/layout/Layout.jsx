4a93dde918ae707c18423267422eaf8313d5dc37
Neeraj <neerajsaraswat111@gmail.com>
2025-03-10 10:15:43 +0530

    made some changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect, axios, server } from "..imports.jsx";
+import { useState, useEffect, axios, server } from "../imports.jsx";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
